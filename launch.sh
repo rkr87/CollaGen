00db4f903d6c64bb5af0a28845a4143b3121f76c
rkr87 <43894515+rkr87@users.noreply.github.com>
2024-09-13 09:33:03 +0100

    fix: #23/bundle termsp with app (#24)

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+export LD_LIBRARY_PATH=/mnt/SDCARD/System/lib:/lib:/usr/lib:/usr/trimui/lib:/mnt/SDCARD/Apps/CollaGen/lib
+export PATH="/bin:/usr/bin:/usr/local/bin:/mnt/SDCARD/System/bin"
 appDir="$(cd "$(dirname "$0")" && pwd)"
-/mnt/SDCARD/Apps/Terminal/SimpleTerminal -e "$appDir/collagen.sh"
+"$appDir/TermSP" -k -f "$appDir/fonts/DejaVuSansMono.ttf" -b "$appDir/fonts/DejaVuSansMono-Bold.ttf" -s 20 -e "$appDir/collagen.sh"
